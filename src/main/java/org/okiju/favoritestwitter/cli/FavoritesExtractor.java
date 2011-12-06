05292969ee94277a0ded275b9d0ad2b1dfbb11b1
Abel Cuenca <abel.cuenca@gmail.com>
2011-12-06 20:31:03 +0100

    doing some refactoring

--- src/main/java/org/okiju/favoritestwitter/cli/FavoritesExtractor.java
+++ src/main/java/org/okiju/favoritestwitter/cli/FavoritesExtractor.java
@@ -1,38 +1,50 @@
 package org.okiju.favoritestwitter.cli;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
+import org.okiju.favoritestwitter.Generator;
 import org.okiju.favoritestwitter.MessageGenerator;
-import org.okiju.favoritestwitter.TwitterHelper;
+import org.okiju.favoritestwitter.TwitterGenerator;
 import org.okiju.favoritestwitter.util.DateFormatter;
 import org.okiju.favoritestwitter.util.EmailBean;
 import org.okiju.favoritestwitter.util.FileHelper;
 import org.okiju.favoritestwitter.util.MailHelper;
 
 /**
- * Hello world!
+ * CLI of favorites extractor
  * 
  */
 public class FavoritesExtractor {
 
     public static void main(String[] args) {
+        checkArgs(args);
+
+        String path = args[0];
+        Properties props = PropertyHelper.loadProperties(path);
+
+        List<String> entries = generateEntries(new TwitterGenerator(props), "ficheroTwits");
+
+        sendEmail(props, entries, "Favoritos del día ");
+    }
+
+    private static void checkArgs(String[] args) {
         if (args.length < 1) {
             System.out.println("Use: FavoritesExtractor configPath");
             System.exit(1);
         }
-        String path = args[0];
-        Properties props = PropertyHelper.loadProperties(path);
+    }
 
-        List<String> entries = TwitterHelper.generateListTwits(props);
-        String prefix = "ficheroTwits";
+    private static List<String> generateEntries(Generator twitterGenerator, String prefix) {
+        List<String> entries = twitterGenerator.generate();
         FileHelper.writeCollectionInDatedFile(prefix, entries);
+        return entries;
+    }
 
+    private static void sendEmail(Properties props, List<String> entries, String prefix) {
         EmailBean emailBean = new EmailBean(props);
         String message = MessageGenerator.generateMessage(entries);
-        String subject = "Favoritos del día " + DateFormatter.formatToday();
+        String subject = prefix + DateFormatter.formatToday();
 
         MailHelper.sendMessage(message, subject, emailBean);
     }
